b88373b54e382f7df67cc6d70a11b2229e6f1581
unknown <kartikbansal2000@gmail.com>
2021-06-26 10:58:19 +0530

    createMail done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import { AuthProvider } from "./contexts/AuthContext";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LogIn from "./components/LogIn";
 import "./App.css";
 import CreateMail from "./pages/createMail/createMail";
-
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-          <Switch>
-            <Route exact path="/" />
-            <Route path="/login" component={LogIn} />
-            
-          </Switch>
-        </Router>
-      <div className="App">
-      <h1> hello</h1>
-      </div>
-
-
-
-      <CreateMail />
-    
+        <Switch>
+          <Route exact path="/" />
+          <Route path="/login" component={LogIn} />
+          <Route path="/CreateMail" component={CreateMail} />
+        </Switch>
+      </Router>
     </AuthProvider>
-    
   );
 }
 
